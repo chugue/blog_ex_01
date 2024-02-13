b6447d069a053d83b46ae87cbe11891cc819bb99
AI_Nomads <chugue85@gmail.com>
2024-02-13 09:15:21 +0900

    213 start

---
+++ src/main/java/shop/mtcoding/blog/board/Board.java
@@ -0,0 +1,23 @@
+package shop.mtcoding.blog.board;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Table(name = "board_tb")
+@Data
+@Entity
+public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(length = 20)
+    private String title;
+    @Column(length = 500)
+    private String content;
+    private int userId;
+    private LocalDateTime createdAt;
+
+}
